2aa6fbf84fcce2842765cd8a938e3b44c081d27f
Sebastyan777 <joglle95@gmail.com>
2024-01-08 18:03:40 +0500

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_8
+{
+    internal class Program
+    {
+        private static int _buffer = 1;
+        private static double _bufferDouble = 1.1d;
+        private static float _bufferFloat = 1.1f;
+        private static char _bufferChar = '1';
+        private static string _bufferString = "1";
+        private static bool _bufferBool = false;
+        static void Main(string[] args)
+        {
+            var buf = (int)_bufferDouble;
+            Console.WriteLine(buf);
+        }
+    }
+}

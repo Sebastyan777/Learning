952c809d33f469368d0f0e28ecc186a9458e8d5b
Sebastyan777 <joglle95@gmail.com>
2024-01-08 18:13:04 +0500

    Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example("Hello", "World");
+        }
+
+        private static void Example(string line1, string line2)
+        {
+            Console.WriteLine($"{line1} {line2}");
+            Example(line1);
+        }
+
+        private static void Example(string line1)
+        {
+            Console.WriteLine($"{line1}");
+        }
+
+        private static string Example(string line1, string line2, string line3)
+        {
+            return line1 + line2 + line3;
+        }
+    }
+}

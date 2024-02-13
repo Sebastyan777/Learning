3021b84bc94d7709ca37b51ecc5d26af0304027f
Sebastyan777 <joglle95@gmail.com>
2024-02-13 12:22:55 +0500

    Task_22

---
+++ Task_22/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_22
+{
+    internal class Program
+    {
+        private static int[][] array = new int[3][];
+
+        static void Main()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new int[3];
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = i * 3 + j + 1;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

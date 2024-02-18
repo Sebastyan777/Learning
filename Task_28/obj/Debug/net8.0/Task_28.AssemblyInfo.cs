6b2f53793425a194fad47213616b42693cc15039
Sebastyan777 <joglle95@gmail.com>
2024-02-18 11:40:18 +0500

    Task_28

--- Task_28/obj/Debug/net8.0/Task_28.AssemblyInfo.cs
+++ Task_28/obj/Debug/net8.0/Task_28.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task_27")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task_28")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task_27")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task_27")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task_28")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task_28")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
